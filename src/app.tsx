fbf50ac0db836ad9ff2872c17c53081e92e73824
Lucas Fidelis <lucasm241301@gmail.com>
2024-07-21 17:31:10 -0300

    Insere funcionalodade de pesquisa

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { ChevronDown, Moon, Pencil, Plus, Save, Search, SunDim, Trash2, X } from 'lucide-react'
-import { useCallback, useState } from "react"
+import { FormEvent, useCallback, useState } from "react"
 
 export function App() {
   const [isBlackModeActive, setIsBlackModeActive] = useState(true)
@@ -7,6 +7,7 @@ export function App() {
   const [isAddActivityModalOpen, setIsAddActivityModalOpen] = useState(false)
   const [editId, setEditId] = useState<number | null>(null)
   const [editTitle, setEditTitle] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
   const [activities, setActivities] = useState([
     {
       id: 1,
@@ -44,6 +45,29 @@ export function App() {
     setIsAddActivityModalOpen(false)
   }
 
+  const addActivity = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const title = data.get('title')?.toString()
+    
+    if (!title){
+      return
+    }
+    const newActivity = {
+      id: activities.length + 1,
+      title,
+      date: new Date().toISOString(),
+      finally: false
+    }
+    
+    setActivities([
+      ...activities,
+      newActivity
+    ])
+    closeAddActivityModal()
+
+  }, [])
+
   const completeActivity = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target
     const idThisInput = parseInt(input.value)
@@ -79,22 +103,39 @@ export function App() {
     closeEditModal()
   }, [editId, editTitle, closeEditModal])
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredActivities = useCallback(() => {
+    return activities.filter(activity => activity.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  }, [searchTerm, activities])
+
   return (
     <div className={`h-screen w-screen flex flex-wrap items-start justify-center transition-colors ${isBlackModeActive ? 'bg-zinc-800 text-zinc-200' : 'bg-zinc-200 text-zinc-900'}`}>
       <div className="w-9/12 flex flex-col gap-5">
         <div className="w-full flex flex-col justify-center items-center gap-3 py-8">
           <h1 className=''>TODO LIST</h1>
           <div className="flex flex-wrap-reverse justify-between w-full gap-5">
-            <form className='flex-1 min-w-full sm:min-w-min flex justify-between bg-transparent border border-indigo-300 rounded-lg p-1.5'>
-              <input type="text" className='flex-1 bg-transparent outline-none' />
+            <div className='flex-1 min-w-full sm:min-w-min flex justify-between bg-transparent border border-indigo-300 rounded-lg p-1.5'>
+              <input 
+                type="text"
+                onChange={handleSearchChange}
+                className='flex-1 bg-transparent outline-none' />
               <button>
                 <Search />
               </button>
-            </form>
-            <button className='h-9 w-20 flex items-center justify-center gap-3 bg-indigo-700 text-zinc-200 rounded-xl'>
-              <h2>ALL</h2>
-              <ChevronDown className='size-5' />
-            </button>
+            </div>
+            <div className='h-9 w-20 flex items-center justify-center gap-1 px-2 bg-indigo-700 text-zinc-200 rounded-xl'>
+              <select className='w-full'>
+                <option value="">ALL</option>
+                <option value="">OPEN</option>
+                <option value="">CLOSE</option>
+                <button>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</button>
+              </select>
+              <ChevronDown className='size-5 text-zinc-200' />
+            </div>
+    
             {isBlackModeActive ? (
               <button
                 onClick={changeThemeScreen}
@@ -112,9 +153,9 @@ export function App() {
         </div>
 
         <div className='w-full'>
-          {activities.length > 0 ? (
+          {filteredActivities().length > 0 ? (
             <div className='flex flex-col gap-3'>
-              {activities.map(activity => (
+              {filteredActivities().map(activity => (
                 <div key={activity.id} className="flex justify-between">
                   <div className='relative flex-1'>
                     <input
@@ -128,7 +169,7 @@ export function App() {
                       <div className="flex gap-3">
                         {activity.finally ? (
                           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#6C63FF" stroke="#6C63FF" />
+                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#4338ca" stroke="#4338ca" />
                             <mask id="path-2-inside-1_0_1" fill="white">
                               <path d="M10.9978 18.6488L6.00002 13.7476L15.5593 4L20.5571 8.90124L10.9978 18.6488Z" />
                             </mask>
@@ -136,7 +177,7 @@ export function App() {
                           </svg>
                         ) : (
                           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#6C63FF" />
+                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#4338ca" />
                           </svg>
                         )}
                         <h2 className='w-36 sm:w-auto'>{activity.title}</h2>
@@ -231,11 +272,12 @@ export function App() {
 
       {isAddActivityModalOpen && (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-          <div className={`sm:w-[450px] py-5 px-8 space-y-10 rounded-md ${isBlackModeActive ? 'bg-zinc-800' : 'bg-zinc-200'}`}>
+          <form onSubmit={addActivity} className={`sm:w-[450px] py-5 px-8 space-y-10 rounded-md ${isBlackModeActive ? 'bg-zinc-800' : 'bg-zinc-200'}`}>
             <div className='flex flex-col items-center gap-2'>
               <h2>NEW NOTE</h2>
               <input
                 type="text"
+                name='title'
                 placeholder='Input your note...'
                 className='w-full'
               />
@@ -249,14 +291,14 @@ export function App() {
                 <X />
               </button>
               <button
-                onClick={saveEdit}
+                type='submit'
                 className='flex gap-2 bg-indigo-700 text-zinc-200 rounded-xl p-2'
               >
                 <h2 className='hidden sm:block'>APPLY</h2>
                 <Plus />
               </button>
             </div>
-          </div>
+          </form>
         </div>
       )}
     </div>
