e9986ee104e3628332b212b0f02c6446fe5a3ecf
Lucas Fidelis <lucasm241301@gmail.com>
2024-07-21 22:09:16 -0300

    Alteração para fonte Kanit

--- src/app.tsx
+++ src/app.tsx
@@ -110,7 +110,7 @@ export function App() {
     <div className={`min-h-screen max-w-screen flex flex-wrap items-start justify-center transition-colors ${isBlackModeActive ? 'bg-zinc-800 text-zinc-200' : 'bg-zinc-200 text-zinc-900'}`}>
       <div className="w-9/12 flex flex-col gap-5">
         <div className="w-full flex flex-col justify-center items-center gap-3 py-8">
-          <h1 className=''>TODO LIST</h1>
+          <h1 className='text-2xl font-semibold'>TODO LIST</h1>
           <div className="flex flex-wrap-reverse justify-between w-full gap-5">
             <div className='flex-1 min-w-full sm:min-w-min flex justify-between bg-transparent border border-indigo-300 rounded-lg p-1.5'>
               <input
@@ -126,9 +126,24 @@ export function App() {
                 value={filter}
                 onChange={handleFilterChange}
                 className='h-9 w-22 flex items-center justify-center px-2 bg-indigo-600 hover:bg-indigo-700 text-zinc-200 rounded-xl cursor-pointer'>
-                <option value="">ALL</option>
-                <option value="Complete">Complete</option>
-                <option value="Incomplete">Incomplete</option>
+                <option 
+                  value=""
+                  className='text-indigo-600 bg-zinc-200'
+                >
+                  ALL
+                </option>
+                <option 
+                  value="Complete"
+                  className='text-indigo-600 bg-zinc-200'
+                >
+                  Complete
+                </option>
+                <option 
+                  value="Incomplete"
+                  className='text-indigo-600 bg-zinc-200'
+                >
+                  Incomplete
+                </option>
               </select>
             </div>
 
@@ -152,48 +167,51 @@ export function App() {
           {filteredActivities().length > 0 ? (
             <div className='flex flex-col gap-3'>
               {filteredActivities().map(activity => (
-                <div key={activity.id} className="flex justify-between">
-                  <div className='relative flex-1'>
-                    <input
-                      onChange={completeActivity}
-                      type="checkbox"
-                      checked={activity.finally}
-                      value={activity.id}
-                      className='w-full absolute inset-0 $'
-                    />
-                    <div className="flex justify-between">
-                      <div className="flex gap-3">
-                        {activity.finally ? (
-                          <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#4f46e5" stroke="#4f46e5" />
-                            <mask id="path-2-inside-1_0_1" fill="white">
-                              <path d="M10.9978 18.6488L6.00002 13.7476L15.5593 4L20.5571 8.90124L10.9978 18.6488Z" />
-                            </mask>
-                            <path d="M10.9978 18.6488L9.59745 20.0767L11.0254 21.4771L12.4257 20.0491L10.9978 18.6488ZM12.3982 17.2209L7.40037 12.3196L4.59966 15.1755L9.59745 20.0767L12.3982 17.2209ZM19.1291 7.50089L9.56986 17.2484L12.4257 20.0491L21.985 10.3016L19.1291 7.50089Z" fill="#F7F7F7" mask="url(#path-2-inside-1_0_1)" />
-                          </svg>
-                        ) : (
-                          <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#4f46e5" />
-                          </svg>
-                        )}
-                        <h2 className='w-36 sm:w-auto'>{activity.title}</h2>
+                <div key={activity.id} className="flex flex-col gap-3 font-semibold">
+                  <div className='flex'>
+                    <div className='relative flex-1'>
+                      <input
+                        onChange={completeActivity}
+                        type="checkbox"
+                        checked={activity.finally}
+                        value={activity.id}
+                        className='w-full absolute inset-0 $'
+                      />
+                      <div className="flex justify-between">
+                        <div className="flex gap-3">
+                          {activity.finally ? (
+                            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#4f46e5" stroke="#4f46e5" />
+                              <mask id="path-2-inside-1_0_1" fill="white">
+                                <path d="M10.9978 18.6488L6.00002 13.7476L15.5593 4L20.5571 8.90124L10.9978 18.6488Z" />
+                              </mask>
+                              <path d="M10.9978 18.6488L9.59745 20.0767L11.0254 21.4771L12.4257 20.0491L10.9978 18.6488ZM12.3982 17.2209L7.40037 12.3196L4.59966 15.1755L9.59745 20.0767L12.3982 17.2209ZM19.1291 7.50089L9.56986 17.2484L12.4257 20.0491L21.985 10.3016L19.1291 7.50089Z" fill="#F7F7F7" mask="url(#path-2-inside-1_0_1)" />
+                            </svg>
+                          ) : (
+                            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#4f46e5" />
+                            </svg>
+                          )}
+                          <h2 className='w-36 sm:w-auto'>{activity.title}</h2>
+                        </div>
                       </div>
                     </div>
+                    <div className="space-x-2 text-zinc-600/60">
+                      <button
+                        onClick={() => deleteActivity(activity.id)}
+                        className='hover:text-red-500'
+                      >
+                        <Trash2 />
+                      </button>
+                      <button
+                        onClick={() => openEditModal(activity.id, activity.title)}
+                        className='hover:text-indigo-600'
+                      >
+                        <Pencil />
+                      </button>
+                    </div>
                   </div>
-                  <div className="space-x-2 text-zinc-600/60">
-                    <button
-                      onClick={() => deleteActivity(activity.id)}
-                      className='hover:text-red-500'
-                    >
-                      <Trash2 />
-                    </button>
-                    <button
-                      onClick={() => openEditModal(activity.id, activity.title)}
-                      className='hover:text-indigo-600'
-                    >
-                      <Pencil />
-                    </button>
-                  </div>
+                  <div className="w-full h-px bg-indigo-500/30"></div>
                 </div>
               ))}
             </div>
