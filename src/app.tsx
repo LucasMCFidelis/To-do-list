f2653a74701ed60c487c9571dad353691f220e88
Lucas Fidelis <lucasm241301@gmail.com>
2024-07-21 21:27:25 -0300

    Alteração na estilização dos botões e dos eventos hover

--- src/app.tsx
+++ src/app.tsx
@@ -96,10 +96,10 @@ export function App() {
 
   const filteredActivities = useCallback(() => {
     return activities.filter(activity => {
-      if (filter === 'OPEN') {
+      if (filter === 'Complete') {
         return !activity.finally;
       }
-      if (filter === 'CLOSE') {
+      if (filter === 'Incomplete') {
         return activity.finally;
       }
       return true;
@@ -125,23 +125,23 @@ export function App() {
               <select
                 value={filter}
                 onChange={handleFilterChange}
-                className='h-9 w-22 flex items-center justify-center px-2 bg-indigo-700 text-zinc-200 rounded-xl'>
+                className='h-9 w-22 flex items-center justify-center px-2 bg-indigo-600 hover:bg-indigo-700 text-zinc-200 rounded-xl cursor-pointer'>
                 <option value="">ALL</option>
-                <option value="OPEN">OPEN</option>
-                <option value="CLOSE">CLOSE</option>
+                <option value="Complete">Complete</option>
+                <option value="Incomplete">Incomplete</option>
               </select>
             </div>
 
             {isBlackModeActive ? (
               <button
                 onClick={changeThemeScreen}
-                className='flex justify-center items-center bg-indigo-700 h-9 w-9 rounded-xl '>
+                className='flex justify-center items-center bg-indigo-600 h-9 w-9 rounded-xl hover:bg-indigo-700'>
                 <SunDim className='size-8' />
               </button>
             ) : (
               <button
                 onClick={changeThemeScreen}
-                className='flex justify-center items-center bg-indigo-700 text-zinc-200 h-9 w-9 rounded-xl'>
+                className='flex justify-center items-center bg-indigo-600 hover:bg-indigo-700 text-zinc-200 h-9 w-9 rounded-xl'>
                 <Moon className='size-8' />
               </button>
             )}
@@ -165,7 +165,7 @@ export function App() {
                       <div className="flex gap-3">
                         {activity.finally ? (
                           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#4338ca" stroke="#4338ca" />
+                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" fill="#4f46e5" stroke="#4f46e5" />
                             <mask id="path-2-inside-1_0_1" fill="white">
                               <path d="M10.9978 18.6488L6.00002 13.7476L15.5593 4L20.5571 8.90124L10.9978 18.6488Z" />
                             </mask>
@@ -173,21 +173,23 @@ export function App() {
                           </svg>
                         ) : (
                           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#4338ca" />
+                            <rect x="0.5" y="0.5" width="25" height="25" rx="1.5" stroke="#4f46e5" />
                           </svg>
                         )}
                         <h2 className='w-36 sm:w-auto'>{activity.title}</h2>
                       </div>
                     </div>
                   </div>
-                  <div className="space-x-2">
+                  <div className="space-x-2 text-zinc-600/60">
                     <button
                       onClick={() => deleteActivity(activity.id)}
+                      className='hover:text-red-500'
                     >
                       <Trash2 />
                     </button>
                     <button
                       onClick={() => openEditModal(activity.id, activity.title)}
+                      className='hover:text-indigo-600'
                     >
                       <Pencil />
                     </button>
@@ -206,7 +208,7 @@ export function App() {
         <div className='flex justify-end'>
           <button
             onClick={openAddActivityModal}
-            className='bg-indigo-700 text-zinc-200 rounded-full p-2'>
+            className='bg-indigo-600 hover:bg-indigo-700 text-zinc-200 rounded-full p-2'>
             <Plus className='size-8' />
           </button>
         </div>
@@ -249,14 +251,14 @@ export function App() {
             <div className="w-full flex justify-between font-semibold">
               <button
                 onClick={closeEditModal}
-                className='flex gap-2 border border-indigo-700 text-indigo-700 rounded-xl p-2'
+                className='flex gap-2 border border-indigo-600 text-indigo-600 hover:border-indigo-700 hover:text-indigo-700 rounded-xl p-2'
               >
                 <h2 className='hidden sm:block'>CANCEL</h2>
                 <X />
               </button>
               <button
                 onClick={saveEdit}
-                className='flex gap-2 bg-indigo-700 rounded-xl p-2'
+                className='flex gap-2 bg-indigo-600 hover:bg-indigo-700 text-zinc-200 rounded-xl p-2'
               >
                 <h2 className='hidden sm:block'>SAVE</h2>
                 <Save />
@@ -275,20 +277,21 @@ export function App() {
                 type="text"
                 name='title'
                 placeholder='Input your note...'
+                required
                 className='w-full'
               />
             </div>
             <div className="w-full flex flex-wrap justify-between font-semibold">
               <button
                 onClick={closeAddActivityModal}
-                className='flex gap-2 border border-indigo-700 text-indigo-700 rounded-xl p-2'
+                className='flex gap-2 border border-indigo-600 text-indigo-600 hover:border-indigo-700 hover:text-indigo-700 rounded-xl p-2'
               >
                 <h2 className='hidden sm:block'>CANCEL</h2>
                 <X />
               </button>
               <button
                 type='submit'
-                className='flex gap-2 bg-indigo-700 text-zinc-200 rounded-xl p-2'
+                className='flex gap-2 bg-indigo-600 hover:bg-indigo-700 text-zinc-200 rounded-xl p-2'
               >
                 <h2 className='hidden sm:block'>APPLY</h2>
                 <Plus />
